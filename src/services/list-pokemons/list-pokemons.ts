1de3532953a57f255c32d35ea221d632b4d34957
Allan Ramos <allangrds@gmail.com>
2023-03-06 20:04:46 -0300

    fix: changing fetchListPokemons interface

--- src/services/list-pokemons/list-pokemons.ts
+++ src/services/list-pokemons/list-pokemons.ts
@@ -1,10 +1,18 @@
 import { Api, PokemonsResponse } from '../../types'
 
+type Parameters = {
+  limit?: number
+  offset?: number
+}
+
 export const listPokemons = (
   api: Api,
-  limit = 5,
-  offset = 0
+  parameters: Parameters = {
+    limit: 5,
+    offset: 0,
+  }
 ): Promise<PokemonsResponse> => {
+  const { limit, offset } = parameters
   const endpoint = `pokemon?offset=${offset}&limit=${limit}`
 
   return api.get(endpoint)
